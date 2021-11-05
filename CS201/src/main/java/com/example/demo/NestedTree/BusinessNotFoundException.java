26b83624e1c16b91b38e6ffee572196c8e8db794
Jasmine Quek <jasminequek.2020@sis.smu.edu.sg>
2021-11-05 16:08:31 +0800

    everything

--- CS201/src/main/java/com/example/demo/NestedTree/BusinessNotFoundException.java
+++ CS201/src/main/java/com/example/demo/NestedTree/BusinessNotFoundException.java
@@ -1,4 +1,4 @@
-package com.example.demo.NestedTree;
+package com.example.demo.nestedtree;
 
 public class BusinessNotFoundException extends RuntimeException{
     public BusinessNotFoundException(){
--- CS201/src/main/java/com/example/demo/multitrees/MultiTreemap2.java
+++ CS201/src/main/java/com/example/demo/multitrees/MultiTreemap2.java
@@ -10,16 +10,85 @@ import java.util.Map;
 import java.util.TreeMap;
 import java.util.Map.Entry;
 
-public class Filter implements Serializable {
+public class MultiTreemap2 implements Serializable { // creates all treemap once initialised
     private static final long serialVersionUID = 1L;
     private FeatureTree trees;
-    // private String state;
-    // private String city;
-    // private Float stars;
-    // private int reviewCount;
+    private Map<Float, ArrayList<Business>> starsTreemap;
+    private Map<Integer, ArrayList<Business>> reviewCountTreemap;
+    private Map<String, ArrayList<Business>> stateTreemap;
+    private Map<String, Map<String, ArrayList<Business>>> stateCityTreemap;
 
-    public Filter() {
-        this.trees = new FeatureTree();
+    public MultiTreemap2(String filepath) {
+        this.trees = new FeatureTree(filepath);
+        starsTreemap = trees.createStarsTreeMap();
+        reviewCountTreemap = trees.createReviewCountTreeMap();
+        stateTreemap = trees.createStateTreeMap();
+        stateCityTreemap = trees.createStateCityTreeMap();
+    }
+
+    public ArrayList<Business> filterAll(Float minStar, int minReview, String state, String city) {
+        ArrayList<Business> toReturn = new ArrayList<Business>();
+        ArrayList<Business> fitsStarsList;
+        ArrayList<Business> fitsReviewCountList;
+        ArrayList<Business> fitsStateList;
+        ArrayList<Business> fitsStateCityList;
+
+        if (minStar > 0) {
+            fitsStarsList = new ArrayList<Business>();
+            for (Entry<Float, ArrayList<Business>> entry : starsTreemap.entrySet()) {
+                if (entry.getKey() >= minStar) {
+                    for (Business b: entry.getValue()) {
+                        fitsStarsList.add(b);
+                    }
+                }
+            }
+        } else {
+            fitsStarsList = null;
+        }
+
+        if (minReview > 0) {
+            fitsReviewCountList = new ArrayList<Business>();
+            for (Entry<Integer, ArrayList<Business>> entry : reviewCountTreemap.entrySet()) {
+                if (entry.getKey() >= minReview) {
+                    for (Business b: entry.getValue()) {
+                        fitsReviewCountList.add(b);
+                    }
+                }
+            }
+        } else {
+            fitsReviewCountList = null;
+        }
+
+        if (state != null && city == null) {
+            fitsStateList = new ArrayList<Business>();
+            fitsStateCityList = null;
+            for (Entry<String, ArrayList<Business>> entry : stateTreemap.entrySet()) {
+                if (entry.getKey().equals(state)) {
+                    for (Business b: entry.getValue()) {
+                        fitsStateList.add(b);
+                    }
+                }
+            }
+        } else if (state != null && city != null) {
+            fitsStateList = null;
+            fitsStateCityList = new ArrayList<Business>();
+            for (Entry<String, Map<String, ArrayList<Business>>> stateEntry : stateCityTreemap.entrySet()) {
+                if (stateEntry.getKey().equals(state)) {
+                    for (Entry<String, ArrayList<Business>> cityEntry : stateEntry.getValue().entrySet()) {
+                        if (cityEntry.getKey().equals(city)) {
+                            for (Business b: cityEntry.getValue()) {
+                                fitsStateCityList.add(b);
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            fitsStateList = null;
+            fitsStateCityList = null;
+        }
+
+        if ()
     }
 
     public ArrayList<Business> fitsStarsList(Float requiredStars) {
@@ -118,7 +187,7 @@ public class Filter implements Serializable {
         return true;
     }
 
-    public ArrayList<Business> fitsAllList(Float requiredStars, int requiredReviewCount, String requiredState, String requiredCity) {
+    public ArrayList<Business> filterAll(Float requiredStars, int requiredReviewCount, String requiredState, String requiredCity) {
         ArrayList<Business> fitsAllList = new ArrayList<Business>();
         
         if (requiredStars != null && requiredReviewCount == 0) {
