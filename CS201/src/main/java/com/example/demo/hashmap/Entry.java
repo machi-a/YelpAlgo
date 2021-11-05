d74ec85d2fa0571bd1d06a8067dec834de5676cf
juzli_ <lixiang.lxw@gmail.com>
2021-11-05 15:56:56 +0800

    try1

---
+++ CS201/src/main/java/com/example/demo/hashmap/Entry.java
@@ -0,0 +1,15 @@
+package com.example.demo.hashmap;
+
+public interface Entry<K,V> {
+  /**
+   * Returns the key stored in this entry.
+   * @return the entry's key
+   */
+  K getKey();
+
+  /**
+   * Returns the value stored in this entry.
+   * @return the entry's value
+   */
+  V getValue();
+}
